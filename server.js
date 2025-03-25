b76be72c31cc0d4ed31ca175a89fcc526bcf1b09
Salesman <samyberri7@gmail.com>
2025-03-25 06:40:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Serve static files from the "public" folder
 app.use(express.static('public'));
 
+// Serve index.html as the main page
 app.get('/', (_, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Health check route
 app.get('/health', (_, res) => {
     res.status(200).send('OK');
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
